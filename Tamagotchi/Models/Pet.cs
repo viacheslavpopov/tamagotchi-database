e41dfece148993d70af065faeb7446fd586a57af
agatakolohe <agatakolohe@gmail.com>
2020-12-16 10:51:28 -0800

    create pet class and ctor
    Co-authored-by: Danielle Thompson <danithompson74@gmail.com>

--- Tamagotchi/Models/Pet.cs
+++ Tamagotchi/Models/Pet.cs
@@ -2,6 +2,17 @@ namespace Tamagotchi.Models
 {
     public class Pet
     {
-        //public string getter and setters
+        public string Name { get; set; }
+        public int Id { get; }
+        public int Food { get; set; } = 100;
+        public int Life { get; set; } = 100;
+        public Pet(string name)
+        {
+            Name = name;
+            Id = 1; //_instances.Count;
+            Food = food;
+            Life = life;
+        }
+
     }
 }
--- Tamagotchi/Startup.cs
+++ Tamagotchi/Startup.cs
@@ -35,7 +35,7 @@ namespace Tamagotchi
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Uh-oh, something went wrong!");
             });
         }
     }
