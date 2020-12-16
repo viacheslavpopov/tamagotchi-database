4915623b7265a321393d900dcdf5af54f4a557bd
agatakolohe <agatakolohe@gmail.com>
2020-12-16 14:27:59 -0800

    test return pet with id method, good fail
    Co-authored-by: Danielle Thompson <danithompson74@gmail.com>

--- Tamagotchi.Tests/ModelTests/PetTests.cs
+++ Tamagotchi.Tests/ModelTests/PetTests.cs
@@ -77,21 +77,21 @@ namespace Tamagotchi.Tests
             Assert.AreEqual(1, result);
         }
 
-        // [TestMethod]
-        // public void Find_ReturnsCorrectItem_Item()
-        // {
-        //   //Arrange
-        //   string description01 = "Walk the dog";
-        //   string description02 = "Wash the dishes";
-        //   Item newItem1 = new Item(description01);
-        //   Item newItem2 = new Item(description02);
+        [TestMethod]
+        public void Find_ReturnsCorrectPet_Pet()
+        {
+            //Arrange
+            string petName01 = "Tomi";
+            string petName02 = "Loganchi";
+            Pet newPet01 = new Pet(petName01);
+            Pet newPet02 = new Pet(petName02);
 
-        //   //Act
-        //   Item result = Item.Find(2);
+            //Act
+            Pet result = Pet.Find(1);
 
-        //   //Assert
-        //   Assert.AreEqual(newItem2, result);
-        // }
+            //Assert
+            Assert.AreEqual(newPet02, result);
+        }
 
     }
 }
