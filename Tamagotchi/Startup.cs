bd1ba4113494bed3a60ced60835e439874e8ec94
ilromali <iliashenkoroman@gmail.com>
2021-01-01 15:19:02 -0800

    Update boilerplate in Startup

--- Tamagotchi/Startup.cs
+++ Tamagotchi/Startup.cs
@@ -44,7 +44,7 @@ namespace Tamagotchi
     
         public static class DBConfiguration
         {
-            public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
+            public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=tamagotchi;";
         }
 
 }
